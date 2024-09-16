d29d4a6862c85bce37665631e4e661059497ccd5
yohantoro <yohantoro.prakasita@gmail.com>
2024-09-16 10:38:30 +0700

    chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { 
+    Inter, 
+    Lusitana,
+    Fira_Sans
+} from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+    weight: ['400'],
+    subsets: ['latin']
+});
+export const firaSans = Fira_Sans({
+    weight: ['400'],
+    subsets: ['latin']
+});
